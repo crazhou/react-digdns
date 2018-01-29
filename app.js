5b5a9994630bc96fe49249ecea85deb0d2b019eb
xiaoxi <crazhou@163.com>
2018-01-29 09:57:23 +0800

    引入Pug模板

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 const oauth = require('./tools/oauth');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redisOption = require('./config/redis');
+const appOption = require('./config/app');
 
+app.set('trust proxy', 1) // Trust first proxy
+
+app.use(session({
+    store: new RedisStore(redisOption),
+    secret:'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure : false, maxAge:null, httpOnly:true}, 
+}))
 
 app.use(express.static('public', {etag:false, maxAge:'1d', index:false}));
 
 app.set('views', './views');
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-    res.render('index');
+    let data = {
+        loginUrl:appOption.oauthURL
+    }
+    res.render('index', data);
 })
 
 app.get('/oauthLogin', function(req, res) {
---
+++ config/redis.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: '127.0.0.1',
+  port: 6379,
+  ttl:20,
+  db: 1,
+  prefix:'sess:'
+};
