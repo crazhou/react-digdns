285b471a72acad2ecf10bf506d4bd2fb4520a4b3
xiaoxi <crazhou@163.com>
2018-02-08 17:54:27 +0800

    增加了一个Enter 事件

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -4,10 +4,16 @@ class AddTodo extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" ref="input" />
+        <input type="text" ref="input" onKeyUp={e => this.handleKeyUp(e)} />
         <button onClick={e => this.handleClick(e)}>Add</button>
       </div>
     );
+  }
+
+  handleKeyUp(e) {
+    if (e.keyCode === 13) {
+      this.handleClick(e);
+    }
   }
 
   handleClick(e) {
